6653bc12a700c3b0311c0b3e1b1f09e8cc41aacb
Ivan-PIA <wind.vanya@gmail.com>
2024-07-19 15:02:03 +0700

    visual

--- python_visual/conv.py
+++ python_visual/conv.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-conv = np.loadtxt("/home/ivan/Desktop/Work_dir/1440/SDR_TX_RX/src/rx/conv.txt")
+conv = np.loadtxt("/home/ivan/Desktop/Work_dir/1440/OFDM_transceiver/src/rx/conv.txt")
 
 
 
 print(conv[:10])
 
 
-for r in range(1000):     #кол-во кадров с графиками
+for r in range(100):     #кол-во кадров с графиками
           # принятые данные помещаем в rx
     plt.clf()           # на каждой итерации очищение старого графика
-    conv = np.loadtxt("/home/ivan/Desktop/Work_dir/1440/SDR_TX_RX/src/rx/conv.txt")
+    conv = np.loadtxt("/home/ivan/Desktop/Work_dir/1440/OFDM_transceiver/src/rx/conv.txt")
     plt.plot(conv)   
     plt.draw()          # пересовываем фигуру
     plt.xlabel('time')
--- python_visual/visual_fft_complex.py
+++ python_visual/visual_fft_complex.py
@@ -3,23 +3,24 @@ import matplotlib.pyplot as plt
 
 
 
+def complex_cpp_file(file):
+
+    with open(file, 'r') as file:
+        data = file.read()
+    # Убираем скобки и разделяем комплексные числа по запятым
+    complex_numbers_str = data.replace('(', '').replace(')', '').split()
+
+    complex_numbers = []
+    for complex_number_str in complex_numbers_str:
+        real_part, imag_part = map(float, complex_number_str.split(','))
+        complex_number = complex(real_part, imag_part)
+        complex_numbers.append(complex_number)
+
+    rx = np.ravel(complex_numbers)
+    return rx
 
 
-
-with open("/home/ivan/Desktop/Work_dir/1440/SDR_TX_RX/src/resurrs/rx_file.txt", 'r') as file:
-    data = file.read()
-
-# Убираем скобки и разделяем комплексные числа по запятым
-complex_numbers_str = data.replace('(', '').replace(')', '').split()
-
-complex_numbers = []
-for complex_number_str in complex_numbers_str:
-    real_part, imag_part = map(float, complex_number_str.split(','))
-    complex_number = complex(real_part, imag_part)
-    complex_numbers.append(complex_number)
-
-rx = np.ravel(complex_numbers)
-
+rx = complex_cpp_file("/home/ivan/Desktop/Work_dir/1440/OFDM_transceiver/src/resurrs/rx_file_copy.txt")
 
 plt.figure(1)
 #plt.title("rx buffer 1 sec(2.5MS) | tx once (5760S)")
