ac52cc47335c1675c3e81c411698a063474c967c
Ivan-PIA <wind.vanya@gmail.com>
2024-11-09 00:08:08 +0700

    add CFO estimation

---
+++ OFDM/freq_offset.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+#define M_PI 3.14159265358979323846
+
+
+void frequency_correlation(const std::vector<std::complex<double>>& pss, const std::vector<std::complex<double>>& matrix_name, \
+double m, std::vector<std::complex<double>>& data_offset, int fs);
